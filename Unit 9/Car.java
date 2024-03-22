e654e2f30b53aa11f942572935bec23557071334
nik807 <122914471+nikhilnunna8@users.noreply.github.com>
2024-03-21 23:27:50 -0500

    Update Car.java
    java doc

--- Unit 9/Car.java
+++ Unit 9/Car.java
@@ -1,3 +1,6 @@
+// Teacher: Mister Anthony DeStephano
+// Students: The Nikhil Nunna, The Mantra Mittal
+// Period 2
 public class Car extends Automobile{
     private boolean sport;
     public Car(double m, String b, double p, boolean s){
@@ -11,6 +14,13 @@ public class Car extends Automobile{
         super(m,b,4,4,p);
         sport = false;
     }
+    /**
+     * Calculates the repair cost for the car based on if the vehicle is a sport vehicle or not.
+     *
+     * @return The repair cost as a double value. If the vehicle was in a crash,
+     *         the repair cost is calculated using the Automobile's repairCost method. If the vehicle
+     *         is a sport vehicle, the repair cost is multiplied by 10.
+     */
     public double repairCost(){
         double x = super.repairCost(super.inCrash());
         if(sport){
@@ -18,6 +28,12 @@ public class Car extends Automobile{
         }
         return x;
     }
+    /**
+     * Returns a string with information about the car using the super getter methods.
+     *
+     * @return A string containing information about the car, including mileage,
+     *         brand, and whether it's a sport car or a regular car.
+     */
     public String toString(){
         String out = "";
         if(sport) {
